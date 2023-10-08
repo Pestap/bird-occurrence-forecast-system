3af3bf6d44a7c2fdbf81897cc547eeb57842b1ee
bartSzczepaniec <bartekssj@icloud.com>
2023-10-08 18:38:20 +0200

    CORS updated

--- server/app.py
+++ server/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from routes.blueprint import blueprint
-
+from flask_cors import CORS, cross_origin
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     return app
 
 
